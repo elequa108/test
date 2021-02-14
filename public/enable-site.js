fd0086336755e6e1606cd6469fcf7d2b31853c65
Kian <56565697+Kian738@users.noreply.github.com>
2021-02-15 00:53:28 +0100

    Delete public directory

--- public/enable-site.js
+++
@@ -1,7 +0,0 @@
-function redirect () {
-  const enabled = true;
-  if (!enabled) {
-    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO";
-    return false;
-  }
-}
